917e746bb9a9c2b36fcdb58a0d94f186b3320c66
mrdanhdanh <nguyenhuudanh9999@gmail.com>
2021-04-15 15:35:34 +0700

    abc

---
+++ js/20880012.js
@@ -0,0 +1,16 @@
+function loadData(request, targetId, sourceId) {
+    var full_request = links + request;
+    $.ajax({
+    url: full_request,
+    cache: false,
+    success: function(data){
+        let jsonData = {
+            data: data
+        };
+        let target = $(targetId);
+        let source = $(sourceId).html();
+        let template = Handlebars.compile(source);
+        $(target).html(template((jsonData)));
+    }
+});
+};
